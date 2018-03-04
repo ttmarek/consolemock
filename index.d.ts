e01a4ad5eaea83e93bb4346fc1ab8acfc8b67eca
Thomas Marek <ttmarek@outlook.com>
2018-03-04 13:15:59 -0500

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+declare function makeConsoleMock(
+  nativeConsole?: Console
+): {
+  history: () => Array<{ [key: string]: any[] }>;
+  printHistory: () => string;
+  clearHistory: () => void;
+  print: (...args: any[]) => void;
+  group: (...title: any[]) => void;
+  groupEnd: () => void;
+  log: (...args: any[]) => void;
+  info: (...args: any[]) => void;
+  warn: (...args: any[]) => void;
+  error: (...args: any[]) => void;
+};
+
+export default makeConsoleMock;
